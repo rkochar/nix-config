b894b85e87417b0fcab3806994fdcfcdef68c95c
Rahul Kochar <rkochar9@gmail.com>
2025-12-27 21:09:11 +0100

    setup ssh

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "wrecker"; # Define your hostname.
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
@@ -35,13 +35,13 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    displayManager.lightdm.enable = true;
-    desktopManager.gnome.enable = true;
-    videoDrivers = [ "fbdev" ];
-  };
+  # Enable the X11 windowing system for startx.
+  # services.xserver = {
+  #   enable = true;
+  #   displayManager.lightdm.enable = true;
+  #   desktopManager.gnome.enable = true;
+  #   videoDrivers = [ "fbdev" ];
+  # };
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -62,12 +62,22 @@
   services.libinput.enable = false;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.rkochar = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
+  users.users = {
+    rkochar = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      packages = with pkgs; [
+        tree
+      ];
+    };
+
+    wrecker = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+	"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDbOEYUNb2XQmkvG8LkXGWMboH9qn19hEYT4VsS+Or72MdQsjFhM6NmlBlUkAGR0bL8PoGB10VPKTydgT9kuX3OmccDqAbeXBP3HGaLhi6dW2AbLwcLO6FYqPba38rH+A+9YgDwvWlQZ/LwPwMK3N30BIe74RdJf2WCF27M6KE7/dLqBwsY2atoY7eQhqOtzXG3rHRBw+YYUbCh50FNY4BmyVzEs5q3WkXJGBeaIwfGaTJPbM3M51btIBOf8RFPCARBh9nSQyD2hEbmvVgh7PspqkMfjlwqETpSsS66pS4Gmr3FLso82WSXq2gP8bzBLMZZ7zg4e+Zt/M7WL8OaVlHh1G6SSDrkrkMg8wCa44NJhpl9Bevw/v41hphAgdiLi5inDKfGHUdfftudBTXG1ijdzhDAkPRfqNs3k+BIrVNe0TJWQpeLljMjZ+0+El7cmxwcMjLgKy6mN9QsFGXMF5VsR6cwpow+ghiA9yLYWiLzh0omJpE0dycYpEnAvnu3tDtWUgoGCvwu7ZXaLZTFfQcLnxqqDbmbw9LhDX9OcrOXu8Jv8cOAtlF102JOkqYUKiKUmy4lKVwPw62WMpGH6XMtKt2f2nsYKEpfYQigJ/NoCY9StOxIeoVTl18pz7F3E4kUP3osDqGXDg2qjDvMYnTuc/M7/YZzd6WXJI2bS+fL+Q== rkochar@rahul-g5"
+      ];
+    };
   };
 
   # programs.firefox.enable = true;
@@ -91,20 +101,22 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    authorizedKeysInHomedir = false;
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = [ "wrecker" ];
+      PermitRootLogin = "no";
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  networking.firewall.enable = false;
 
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
 
