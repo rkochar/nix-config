4e59b98ae09a8024697fb6ef4108e7048ab5ca21
Rahul Kochar <rkochar9@gmail.com>
2026-05-23 19:02:26 +0200

    fix: chromium settings

--- home/rkochar/common/optional/browsers/chromium.nix
+++ home/rkochar/common/optional/browsers/chromium.nix
@@ -1,5 +1,14 @@
+# Migrate to ungoogled-chromium
 {
   programs.chromium = {
     enable = true;
- };
+    extensions = [
+      "eimadpbcbfnmbkopoojfekhnkhdbieeh"  # Dark Reader
+      "bkkbcggnhapdmkeljlodobbkopceiche"  # Popup blocker
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm"  # UBlock Origin
+    ];
+    commandLineArgs = [
+      "--restore-last-session"
+    ];
+  };
 }
