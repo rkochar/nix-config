0642c2656b44bcc52b4432f51084e3212cc68a75
Rahul Kochar <rkochar9@gmail.com>
2026-06-09 22:34:57 +0200

    chore(browsers): add translate to chromium

--- home/rkochar/common/optional/browsers/chromium.nix
+++ home/rkochar/common/optional/browsers/chromium.nix
@@ -1,16 +1,17 @@
 # Migrate to ungoogled-chromium
 {
-  programs.chromium = {
-    enable = true;
-    extensions = [
-      "eimadpbcbfnmbkopoojfekhnkhdbieeh"  # Dark Reader
-      "bkkbcggnhapdmkeljlodobbkopceiche"  # Popup blocker
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm"  # UBlock Origin
-    ];
-    # chrome://flags
-    commandLineArgs = [
-      "--restore-last-session"
-      "--force-dark-mode"
-    ];
-  };
+    programs.chromium = {
+        enable = true;
+        extensions = [
+            "eimadpbcbfnmbkopoojfekhnkhdbieeh"  # Dark Reader
+            "bkkbcggnhapdmkeljlodobbkopceiche"  # Popup blocker
+            "cjpalhdlnbpafiamejdnhcphjbkeiagm"  # UBlock Origin
+            "aapbdbdomjkkjkaonfhkkikfgjllcleb"  # Google Translate
+        ];
+        # chrome://flags
+        commandLineArgs = [
+            "--restore-last-session"
+            "--force-dark-mode"
+        ];
+    };
 }
